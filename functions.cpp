41ac180ee5f92749e4ddd72a2d069aac4fe01619
SaifUllah <bsee25001@itu.edu.pk>
2026-03-29 21:04:51 +0500

    All Tasks done

--- functions.cpp
+++ functions.cpp
@@ -3,10 +3,19 @@
 
 int sumOfSquares(int n) 
 {
+    //base case
+    if(n == 1){
+        return 1;
+    }
+    return n*n+sumOfSquares(n-1);
 }
 
 int findFactorial(int n)
 {
+    if (n == 1){
+        return 1;
+    }
+    return n*findFactorial(n-1);
 }
 
 
--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 
 
 int main() {
+   int var;
+   cout << "Enter the task you want to perform:";
+   cin >> var;
+   switch(var){
+   int n;
+   case 1:
    
+   cout << "Enter the value of n:";
+   cin >> n;
+   cout << "Result:" << sumOfSquares(n);
+   break;
+   case 2:
+   
+   cout << "Enter the value of n:";
+   cin >> n;
+   cout <<  "Result:" << findFactorial(n);
+   break;
+   }
 }
